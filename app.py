cfaedca75fae7b611eb12b621f70786915eb6c78
Amna <151729717+amna2024@users.noreply.github.com>
2024-12-17 21:34:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from urllib.parse import quote as url_quote
- 
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-   return "Hello World!"
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    app.run(debug=True)
