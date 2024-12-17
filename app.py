4cdaaca5d845f7d6790ef007f041e562dd0497ec
Amna <151729717+amna2024@users.noreply.github.com>
2024-12-17 20:09:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask
-
-# Initialize the Flask application
 app = Flask(__name__)
 
-# Define a route for the root URL
 @app.route("/")
 def hello():
-    return "Hello World!"  # Response to GET requests at '/'
+   return "Hello World!"
 
-# Entry point for running the application
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)  # Host the app on all network interfaces
+   app.run(host='0.0.0.0', port=8080)
